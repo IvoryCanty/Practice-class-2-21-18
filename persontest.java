c06f3bac761ac904e724e1490630712f36149dde
Canty <idcanty.2016@mymail.jcsu.edu>
2018-02-21 13:29:34 -0500

    first commit

---
+++ persontest.java
@@ -0,0 +1,10 @@
+
+public class persontest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+person p1 = new person ("jay", 17);
+System.out.println(p1 is Adult());
+	}
+
+}
